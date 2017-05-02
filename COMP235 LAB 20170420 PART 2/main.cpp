a9be9262a910519b77d5875835486adb127e42ed
aberry5621 <aberry5621@gmail.com>
2017-05-02 10:08:28 -0700

    Initial Commit

---
+++ COMP235 LAB 20170420 PART 2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  COMP235 LAB 20170420 PART 2
+//
+//  Created by ax on 5/2/17.
+//  Copyright © 2017 COMP235. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
