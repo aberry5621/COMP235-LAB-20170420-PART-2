9f4e26a8f804f0e1f7db287f9937a1f96baadac6
aberry5621 <aberry5621@gmail.com>
2017-05-23 08:52:39 -0700

    Clean up and test pre-final

--- COMP235 LAB 20170420 PART 2/main.cpp
+++ COMP235 LAB 20170420 PART 2/main.cpp
@@ -1,24 +1,10 @@
-// individual assignment
-
-/*
- except for the use of iterators
- following code test functions
- common to all STL containers
- get rid of hidden rich code
- and run it
- 
- since STL list(doubly linked list
- is a sequence container
- it should work for STL vector
- implemented using dynamic array
- 
- convert the code to test stl vector instead
- and determine any differences
- */
-
-# define prog 3
+# define prog 1
 
 #if(prog == 1)
+
+/*
+1. (Individual Assignment) Except for the use of iterators, the following code test the functions that are common to all STL containers. Get rid of the hidden rich code and run it. Since STL “list” (implemented using doubly-linked list) is a sequence container, it should also work for STL vector (implemented using dynamic array). You will convert the code to test STL vector instead, and determine if there is any differences. Copy and paste your code with the screenshots down below:
+ */
 
 #include <list>
 #include <iostream>
@@ -99,97 +85,11 @@ int main()
     return 0;
 }
 
-#elif(prog == 2)
+#elif (prog == 2)
 
-#include <vector>
-#include <iostream>
-#include <iomanip>
-
-using namespace std;
-
-int main()
-{
-    /*** Test common functions of containers ***/
-    //default constructors
-    vector<int> myVector1;
-    
-    //Get an appropriate iterator for myVector1
-    vector<int>::iterator itr = myVector1.begin();
-    
-    //insert(pos, elem), 'pos' is an iterator
-    myVector1.insert(itr, 111);
-    
-    itr = myVector1.begin();
-    
-    // iterator should break after adding item to vector
-    // so reset it
-    
-    myVector1.insert(itr, 222);
-    
-    itr = myVector1.begin();
-    
-    myVector1.insert(itr, 333);
-    
-    //copy constructor
-    vector<int> myVector2(myVector1);
-    
-    //Get an appropriate iterator for myList1
-    vector<int>::iterator iter2 = myVector2.begin();
-    
-    //erase(begin, end)
-    myVector2.erase(++iter2, myVector2.end());
-    
-    //empty( )
-    cout << boolalpha;
-    if (!myVector1.empty()) cout << "myVector1.empty() returns: " << myVector1.empty() << endl;
-    //size( )
-    if (!myVector1.empty()) cout << "myVector1.size() returns: " << myVector1.size() << endl;
-    if (!myVector2.empty()) cout << "myVector2.size() returns: " << myVector2.size() << endl;
-    //max_size( )
-    if (!myVector1.empty()) cout << "myVector1.max_size() returns: " << myVector1.max_size() << endl;
-    //Question 1: Is MyVector2 a shallow or deep copy of myVector2? Why?
-    
-    //swap( )
-    myVector1.swap(myVector2);
-    
-    if (!myVector1.empty()) cout << "myVector1.size() returns: " << myVector1.size() << endl;
-    cout << "Content of myVector1..." << endl;
-    itr = myVector1.begin();
-    while (itr != myVector1.end())
-    {
-        cout << *itr << ' ';
-        itr++;
-    }
-    cout << endl;
-    
-    if (!myVector2.empty()) cout << "myVector2.size() returns: " << myVector2.size() << endl;
-    cout << "Content of myVector2..." << endl;
-    iter2 = myVector2.begin();
-    while (iter2 != myVector2.end())
-    {
-        cout << *iter2 << ' ';
-        iter2++;
-    }
-    
-    cout << endl;
-    
-    //begin( ) and end ( )
-    //for (list<int>::iterator forwardIter = myList2.begin(); forwardIter != myList2.end(); forwardIter++)
-    cout << "Display vector 2 forward..." << endl;
-    for (auto myForwardIter = myVector2.begin(); myForwardIter != myVector2.end(); myForwardIter++)
-        cout << *myForwardIter << ' ';
-    cout << endl;
-    
-    //rbegin( ) and rend ( )
-    cout << "Display vector 2 in reverse..." << endl;
-    for (auto myReverseIter = myVector2.rbegin(); myReverseIter != myVector2.rend(); myReverseIter++)
-        cout << *myReverseIter << ' ';
-    cout << endl;
-    
-    return 0;
-}
-
-#elif (prog == 3)
+/*
+ 2. (Group assignment – group of 3) The vector code below demonstrated the use of some template functions from the <algorithm> header. Run the code, understand what it does (note that functions are passed as arguments in a couple of examples). Then demonstrate the use of other algorithms (listed below the code below) from the #include <algorithm> header. You should use some online resources to do a little research on your own:
+ */
 
 //Testing functions from the algorithm header
 //Note: New syntax to you, passing functions as arguments, some for_each( ) and generate( )
